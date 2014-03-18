b01b541df4b2d9bf0486c521ef89cf58f7be669d
Stephen <steve@tuxcon.com>
2014-03-18 10:50:04 +1030

    Return list of all files in a python list for later usage

--- dir_list.py
+++ dir_list.py
@@ -1,11 +1,14 @@
 import os
 
 def listing(target):
+    files=[]
     for dirname, dirnames, filenames in os.walk(target):
         # print path to all subdirectories first.
-        for subdirname in dirnames:
-            print(os.path.join(dirname, subdirname))
+        # for subdirname in dirnames:
+        #     print(os.path.join(dirname, subdirname))
 
         # print path to all filenames.
         for filename in filenames:
-            print(os.path.join(dirname, filename))
+            files.append(os.path.join(dirname, filename))
+            # print(os.path.join(dirname, filename))
+    return(files)
