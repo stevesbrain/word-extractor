23fb5ebb3bfb1caeb479553a6120e8f46b9510b4
Stephen <steve@tuxcon.com>
2014-03-18 10:52:34 +1030

    Return all text within files now to stdout

--- documenter.py
+++ documenter.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import sys
 from docx import opendocx, getdocumenttext
+import dir_list
 
-
-document = opendocx(sys.argv[1])
-textlist = getdocumenttext(document)
-print(textlist)
+list = dir_list.listing(sys.argv[1])
+num = 1
+for listing in list:
+    document = opendocx(listing)
+    textlist = getdocumenttext(document)
+    print(textlist)
