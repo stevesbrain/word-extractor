141b750385450453b10d03c0375c0e94fba72f2e
Stephen <steve@tuxcon.com>
2014-03-18 11:01:58 +1030

    Added comments, and README

--- dir_list.py
+++ dir_list.py
@@ -1,14 +1,19 @@
 import os
 
 def listing(target):
+    #Create our list for later returns
     files=[]
+    #For all the stuff in our target
     for dirname, dirnames, filenames in os.walk(target):
-        # print path to all subdirectories first.
+        ## print path to all subdirectories first - commented out because it's not needed
+        ## in our specific project
         # for subdirname in dirnames:
         #     print(os.path.join(dirname, subdirname))
 
-        # print path to all filenames.
+        # print path to all filenames, and append this to our list
         for filename in filenames:
             files.append(os.path.join(dirname, filename))
+            ## Print function below if anyone ever wants the lonely sod
             # print(os.path.join(dirname, filename))
+    #Send back our list for usage
     return(files)
--- documenter.py
+++ documenter.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
+# FIXME Return usage if no argument is provided
+
+#Import our modules
 import sys
+#Grab the important stuff from the docx module
 from docx import opendocx, getdocumenttext
+#Grab our dir_list function
 import dir_list
 
+#List variable is the actual list send back of directories from our dir_list module
 list = dir_list.listing(sys.argv[1])
-num = 1
+
+#For each item in our list, spit out the contents of said document
 for listing in list:
+    #Open the document from listing
     document = opendocx(listing)
+    #Grab text from opened document
     textlist = getdocumenttext(document)
-    print(textlist)
+    #Print text, followed by document title
+    print(textlist, "FILENAME: ", listing)
